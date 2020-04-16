304ac2cc4b4bdb900029973f31ea12139b548242
Rany G <ranywwe@hotmail.com>
2020-04-16 21:08:21 +0200

    Create headerConnect.php
    headerConnect is a header with "se déconnecter" instead of "se connecter | s'inscrire"

---
+++ site/headerConnect.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Index</title>
+    <link rel="stylesheet" href="css/helmet_checkbox.css">
+    <link rel="stylesheet" href="css/home_style.css">
+    <link rel="stylesheet" href="css/inscriptionConnexion_style.css">
+</head>
+
+<body>
+    
+        <header>
+            <div id="deconnexion">
+                <a href="deconnexion.php"> Se déconnecter </a>
+            </div>
+
+            <nav id="headerMenu">
+                <ul>
+                    <li>
+                        <a href="home.php">
+                            <img id="logo" src="img/logo.png" alt="F1-logo" />
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="pronostic.php"> Pronostic </a>
+                    </li>
+
+                    <li>
+                        <a href="teams.php"> Équipes </a>
+                    </li>
+
+                    <li>
+                        <a href="drivers.php"> Pilotes </a>
+                    </li>
+
+                    <li>
+                        <a href="classement.php"> Classement </a>
+                    </li>
+
+                    <li>
+                        <a href="calendrier.php"> Calendrier </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div id="all"> 
