380172fbf310f8d3438561b7ec3a4b39e9c1403a
Rany G <ranywwe@hotmail.com>
2020-04-16 21:09:07 +0200

    Create homeConnect.php
    Including headerConnect.php instead of header.php

---
+++ site/homeConnect.php
@@ -0,0 +1,38 @@
+<?php 
+    require 'headerConnect.php'; 
+?>
+        <div id="content">
+            <div id="preTweets">
+                <div id="presentation">
+                    <p id="presentationF1"> 
+                        La Formule 1 aussi appelé F1, est une des fameuses discipline du sport automobile, elle est aussi considérée comme la catégorie reine de ce sport.
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non vulputate dui. Donec feugiat ligula massa, sed posuere justo auctor ac. Ut tellus orci, ultricies quis tincidunt ut, semper nec metus. Phasellus congue nisl ipsum, malesuada viverra eros pulvinar ut. Duis erat nulla, finibus sit amet fermentum ac, consectetur at neque. Aliquam sodales neque sed orci porttitor finibus. Nunc a odio eget tellus interdum gravida vel non orci. Vestibulum nunc nisi, commodo id erat sed, blandit iaculis augue. Mauris ex sem, convallis porttitor elementum a, rutrum et massa. Etiam porta augue eros, ut aliquet nulla viverra sed. Proin consequat at nisl id gravida. Mauris vel dolor nisi. Donec quis quam bibendum, blandit odio sit amet, ultricies tortor. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non vulputate dui. Donec feugiat ligula massa, sed posuere justo auctor ac. Ut tellus orci, ultricies quis tincidunt ut, semper nec metus. Phasellus congue nisl ipsum, malesuada viverra eros pulvinar ut. Duis erat nulla, finibus sit amet fermentum ac, consectetur at neque. Aliquam sodales neque sed orci porttitor finibus. Nunc a odio eget tellus interdum gravida vel non orci. Vestibulum nunc nisi, commodo id erat sed, blandit iaculis augue. Mauris ex sem, convallis porttitor elementum a, rutrum et massa. Etiam porta augue eros, ut aliquet nulla viverra sed. Proin consequat at nisl id gravida. Mauris vel dolor nisi. Donec quis quam bibendum, blandit odio sit amet, ultricies tortor. 
+                    </p>
+                    <p id="presentationSite">
+                        Praesent urna orci, mollis in nulla eu, efficitur tincidunt lectus. Aliquam erat volutpat. Vivamus posuere ultrices laoreet. Suspendisse at augue eget lectus ullamcorper pulvinar semper sed nisi. Cras semper ornare maximus. Curabitur at lectus a velit euismod accumsan. Mauris lacinia sollicitudin risus vitae iaculis.
+
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec mattis, leo in pellentesque viverra, orci nulla posuere magna, sit amet luctus erat ligula vitae lorem. Nam varius nisl tortor, eget sodales eros posuere nec. Morbi ac odio pharetra, commodo dui non, viverra nulla. Suspendisse porta tellus in consequat sollicitudin. Aenean et tristique nisi. Phasellus ultrices dolor sed dui rutrum, et interdum mauris maximus.
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non vulputate dui. Donec feugiat ligula massa, sed posuere justo auctor ac. Ut tellus orci, ultricies quis tincidunt ut, semper nec metus. Phasellus congue nisl ipsum, malesuada viverra eros pulvinar ut. Duis erat nulla, finibus sit amet fermentum ac, consectetur at neque. Aliquam sodales neque sed orci porttitor finibus. Nunc a odio eget tellus interdum gravida vel non orci. Vestibulum nunc nisi, commodo id erat sed, blandit iaculis augue. Mauris ex sem, convallis porttitor elementum a, rutrum et massa. Etiam porta augue eros, ut aliquet nulla viverra sed. Proin consequat at nisl id gravida. Mauris vel dolor nisi. Donec quis quam bibendum, blandit odio sit amet, ultricies tortor. 
+                        ...
+                    </p>
+                </div>
+                <div id="tweets">
+                    <a class="twitter-timeline" data-width="425" data-height="400" data-theme="light" href="https://twitter.com/F1?ref_src=twsrc%5Etfw">Tweets by F1</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+            </div>
+                
+            <div id="classement">
+                <h2> Classement </h2>
+                <ul id="classementGeneral">
+                    <li> Pilotes </li>
+                    <li> Écuries </li>
+                    <li> Joueurs </li>
+                </ul>
+            </div>
+        </div>    
+<?php
+            require 'footer.php';
+    ?>  
