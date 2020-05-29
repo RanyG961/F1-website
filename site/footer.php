b6048991bd85c9f071db53f48a714c78b3295a7a
Rany G <ranywwe@hotmail.com>
2020-05-29 04:09:26 +0200

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -3,7 +3,7 @@
             <nav id="footerMenu">
                 <ul>
                     <li> <a href="contact.php"> Contact us </a></li>
-                    <li> <span> Site fait par Momo C</span> </li>
+                    <li> <span> Site fait par Rany G.</span> </li>
                 </ul>  
             </nav>
         </footer>
